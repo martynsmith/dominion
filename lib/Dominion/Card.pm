9e054f0c619b8cfb3c280c87d6fea2f3224c3dac
Martyn Smith <martyn@dollyfish.net.nz>
2010-07-25 20:59:19 +1200

    Helper method on base Card class for determining type

--- lib/Dominion/Card.pm
+++ lib/Dominion/Card.pm
@@ -7,5 +7,11 @@ has 'type'      => ( isa => 'Str', is => 'ro', required => 1 );
 has 'set'       => ( isa => 'Str', is => 'ro', required => 1 );
 has 'cost_gold' => ( isa => 'Int', is => 'ro', required => 1 );
 
+sub is {
+    my ($self, $type) = @_;
+
+    return 1 if $self->type eq $type;
+    return;
+}
 #__PACKAGE__->meta->make_immutable;
 1;
