83def8bd3261fd6d4e32cdd26409a230ad85c52b
Martyn Smith <martyn@dollyfish.net.nz>
2010-08-01 19:15:04 +1200

    Implemented Moneylender

--- lib/Dominion/Cards/Moneylender.pm
+++ lib/Dominion/Cards/Moneylender.pm
@@ -9,8 +9,19 @@ sub box         { 'Dominion' }
 sub cost_coin   { 4 }
 sub cost_potion { 0 }
 
-# Trash a Copper from your hand.
-# If you do, +3 Gold.
+sub action {
+    my ($self, $player, $game) = @_;
+
+    # Trash a Copper from your hand.
+    # If you do, +3 Gold.
+
+    my $copper = $player->hand->card_by_name('Copper');
+
+    if ( $copper ) {
+        $game->trash->add($copper);
+        $player->coin_add(3);
+    }
+}
 
 #__PACKAGE__->meta->make_immutable;
 1;
