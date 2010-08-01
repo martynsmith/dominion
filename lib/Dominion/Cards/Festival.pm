b8b66df72f43293d74a7ec74926258483aaf01ff
Martyn Smith <martyn@dollyfish.net.nz>
2010-08-01 19:04:42 +1200

    Implemented Festival

--- lib/Dominion/Cards/Festival.pm
+++ lib/Dominion/Cards/Festival.pm
@@ -9,9 +9,18 @@ sub box         { 'Dominion' }
 sub cost_coin   { 5 }
 sub cost_potion { 0 }
 
-# +2 Actions
-# +1 Buy
-# +2 Gold.
+sub action {
+    my ($self, $player) = @_;
+
+    # +2 actions
+    $player->actions_add(2);
+
+    # +1 buy
+    $player->buys_add(1);
+
+    # +2 coin
+    $player->coin_add(2);
+}
 
 #__PACKAGE__->meta->make_immutable;
 1;
