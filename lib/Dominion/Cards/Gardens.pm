3b497499d95dbe0b267b1c0ad1f2f0e3585af589
Martyn Smith <martyn@dollyfish.net.nz>
2010-08-01 20:12:08 +1200

    Implemented Gardens

--- lib/Dominion/Cards/Gardens.pm
+++ lib/Dominion/Cards/Gardens.pm
@@ -10,6 +10,12 @@ sub cost_coin   { 4 }
 sub cost_potion { 0 }
 
 # Worth 1 Victory for every 10 cards in your deck (rounded down).
+sub victory_points {
+    my ($self, $set) = @_;
+
+    return int($set->count/10);
+}
+
 
 #__PACKAGE__->meta->make_immutable;
 1;
