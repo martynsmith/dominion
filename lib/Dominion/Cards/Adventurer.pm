f52b2e39a12c3c48728b2c8c74a50f1444d55d02
Donovan Jones <donovan.jones@gamma.net.nz>
2010-08-02 17:53:50 +0800

    Implement Adventurer action logic

--- lib/Dominion/Cards/Adventurer.pm
+++ lib/Dominion/Cards/Adventurer.pm
@@ -9,8 +9,33 @@ sub box         { 'Dominion' }
 sub cost_coin   { 6 }
 sub cost_potion { 0 }
 
-# Reveal cards from your deck until you reveal 2 Treasure cards.
-# Put those Treasure cards in your hand and discard the other revealed cards.
+sub action {
+    my ($self, $player, $game) = @_;
+
+    # Reveal cards from your deck until you reveal 2 Treasure cards.
+    # Put those Treasure cards in your hand and discard the other revealed cards.
+
+    my @cards_to_discard;
+
+    my $treasure_count = 0;
+
+    until ($treasure_count == 2) {
+
+        my ($card) = $player->draw(1);
+
+        # check if the card is a treasure card
+        if ( $card->is('treasure') ) {
+            $player->hand->add($card);
+            $treasure_count++;
+        }
+        else {
+            push @cards_to_discard, $card;
+        }
+
+    }
+
+    $player->discard->add(@cards_to_discard);
+}
 
 #__PACKAGE__->meta->make_immutable;
 1;
